cc621b62f136863f68adbb22b7f8b0f097f16cee
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2024-08-30 11:14:05 -0400

    Add BMP3 I2C HAL abstraction driver

---
+++ Core/Inc/bmp3_hal_i2c.h
@@ -0,0 +1,85 @@
+/*******************************************************************************
+ * @file bmp3_hal_i2c.h
+ * @brief BMP3 driver HAL abstraction via I2C.
+ *******************************************************************************
+ */
+
+#ifndef __BMP3_HAL_I2C_H
+#define __BMP3_HAL_I2C_H
+
+/** Includes. *****************************************************************/
+
+#include "bmp3.h"
+
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_i2c.h"
+
+/** STM32 port and pin configs. ***********************************************/
+
+extern I2C_HandleTypeDef hi2c1;
+extern TIM_HandleTypeDef htim4;
+
+// I2C.
+#define BMP3_HI2C hi2c1
+#define BMP3_HTIM htim4
+
+/** Public functions. *********************************************************/
+
+/**
+ * @brief STM32 HAL abstraction initialization.
+ *
+ * @param bmp3: Structure instance of bmp3_dev.
+ * @param intf: Interface selection parameter.
+ *
+ * @return Status of execution
+ * @retval 0 -> Success
+ * @retval < 0 -> Failure Info
+ */
+BMP3_INTF_RET_TYPE bmp3_interface_init(struct bmp3_dev *bmp3, uint8_t intf);
+
+/**
+ * @brief Read the sensor's registers through I2C bus.
+ *
+ * @param reg_addr: Register address.
+ * @param reg_data: Pointer to the data buffer to store the read data.
+ * @param len: Number of bytes to read.
+ * @param intf_ptr: Interface pointer.
+ *
+ * @return Status of execution
+ * @retval = BMP3_INTF_RET_SUCCESS -> Success
+ * @retval != BMP3_INTF_RET_SUCCESS  -> Failure Info
+ */
+BMP3_INTF_RET_TYPE bmp3_i2c_read(uint8_t reg_addr, uint8_t *reg_data,
+                                 uint32_t len, void *intf_ptr);
+
+/**
+ * @brief Writing to the sensor's registers through I2C bus.
+ *
+ * @param reg_addr: Register address.
+ * @param reg_data: Pointer to the data buffer whose value is to be written.
+ * @param len: Number of bytes to write.
+ * @param intf_ptr: Interface pointer.
+ *
+ * @return Status of execution.
+ * @retval = BMP3_INTF_RET_SUCCESS -> Success.
+ * @retval != BMP3_INTF_RET_SUCCESS  -> Failure Info.
+ */
+BMP3_INTF_RET_TYPE bmp3_i2c_write(uint8_t reg_addr, const uint8_t *reg_data,
+                                  uint32_t len, void *intf_ptr);
+
+/**
+ * @brief Blocking delay function for required time in microseconds.
+ *
+ * @param period: Time in microseconds.
+ * @param intf_ptr: Interface pointer.
+ */
+void bmp3_delay_us(uint32_t period, void *intf_ptr);
+
+/**
+ * @brief Process result errors.
+ *
+ * @param result: BMP3 error/result code.
+ */
+void bmp3_result_error_handler(int8_t result);
+
+#endif
---
+++ Core/Src/bmp3_hal_i2c.c
@@ -0,0 +1,104 @@
+/*******************************************************************************
+ * @file bmp3_hal_i2c.c
+ * @brief BMP3 driver HAL abstraction via I2C.
+ *******************************************************************************
+ */
+
+/** Includes. *****************************************************************/
+
+#include "bmp3_hal_i2c.h"
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/** Public variable. **********************************************************/
+
+static uint8_t device_address; // Device I2C address.
+
+/** Definitions. **************************************************************/
+
+#define BMP3_SHUTTLE_ID 0xD3 // BMP3 shuttle board ID.
+
+/** Public functions. *********************************************************/
+
+BMP3_INTF_RET_TYPE bmp3_interface_init(struct bmp3_dev *bmp3, uint8_t intf) {
+  int8_t result = BMP3_OK;
+
+  if (bmp3 != NULL) {
+    if (intf == BMP3_I2C_INTF) {
+      // TODO: NOTE: Bus configuration locked to I2C!
+      device_address = BMP3_ADDR_I2C_PRIM;
+      bmp3->read = bmp3_i2c_read;
+      bmp3->write = bmp3_i2c_write;
+      bmp3->intf = BMP3_I2C_INTF;
+    }
+
+    bmp3->delay_us = bmp3_delay_us;
+    bmp3->intf_ptr = &device_address;
+
+  } else {
+    result = BMP3_E_NULL_PTR;
+  }
+
+  return result;
+}
+
+BMP3_INTF_RET_TYPE bmp3_i2c_read(uint8_t reg_addr, uint8_t *reg_data,
+                                 uint32_t len, void *intf_ptr) {
+  const uint8_t device_addr = *(uint8_t *)intf_ptr;
+  (void)intf_ptr;
+
+  return HAL_I2C_Mem_Read(&BMP3_HI2C, device_addr, reg_addr, 1, reg_data, len,
+                          HAL_MAX_DELAY);
+}
+
+BMP3_INTF_RET_TYPE bmp3_i2c_write(uint8_t reg_addr, const uint8_t *reg_data,
+                                  uint32_t len, void *intf_ptr) {
+  uint8_t device_addr = *(uint8_t *)intf_ptr;
+  (void)intf_ptr;
+
+  return HAL_I2C_Mem_Write(&BMP3_HI2C, device_addr, reg_addr, 1,
+                           (uint8_t *)reg_data, len, HAL_MAX_DELAY);
+}
+
+void bmp3_delay_us(uint32_t period, void *intf_ptr) {
+  (void)intf_ptr;
+
+  __HAL_TIM_SET_COUNTER(&BMP3_HTIM, 0); // Reset the counter.
+  HAL_TIM_Base_Start(&BMP3_HTIM);
+  while (__HAL_TIM_GET_COUNTER(&BMP3_HTIM) < period)
+    ;
+  HAL_TIM_Base_Stop(&BMP3_HTIM);
+}
+
+void bmp3_result_error_handler(const int8_t result) {
+  switch (result) {
+  case BMP3_OK:
+    break;
+  case BMP3_E_NULL_PTR:
+    // TODO: Error handling for null pointer.
+    break;
+  case BMP3_E_COMM_FAIL:
+    // TODO: Error handling for communication failure.
+    break;
+  case BMP3_E_INVALID_LEN:
+    // TODO: Error handling for incorrect length parameter.
+    break;
+  case BMP3_E_DEV_NOT_FOUND:
+    // TODO: Error handling for device not found.
+    break;
+  case BMP3_E_CONFIGURATION_ERR:
+    // TODO: Error handling for configuration Error.
+    break;
+  case BMP3_W_SENSOR_NOT_ENABLED:
+    // TODO: Error handling for warning when Sensor not enabled.
+    break;
+  case BMP3_W_INVALID_FIFO_REQ_FRAME_CNT:
+    // TODO: Error handling for warning Fifo watermark level not in limit.
+    break;
+  default:
+    // TODO: Error handling for unknown error code.
+    break;
+  }
+}
