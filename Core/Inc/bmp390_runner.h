b450f971b334ae117553750868af66c41d3b347d
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2024-08-31 11:25:52 -0400

    Add WIP BMP390 runner driver

---
+++ Core/Inc/bmp390_runner.h
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * @file bmp390_runner.h
+ * @brief BMP390 HAL runner.
+ *******************************************************************************
+ */
+
+#ifndef __BMP390_RUNNER_H
+#define __BMP390_RUNNER_H
+
+/** Includes. *****************************************************************/
+
+#include "bmp3_hal_i2c.h"
+
+#include "bmp3.h"
+
+/** Definitions. **************************************************************/
+
+// Defines frame count requested.
+// As, Pressure and Temperature are enabled in this example:
+// Total byte count requested = FIFO_FRAME_COUNT * BMP3_LEN_P_AND_T_HEADER_DATA.
+#define FIFO_FRAME_COUNT UINT8_C(50)
+
+#define FIFO_MAX_SIZE UINT16_C(512) // Maximum FIFO size.
+
+/** Public functions. *********************************************************/
+
+/**
+ * @brief Get instance of BMP390 with BMP3 driver.
+ *
+ * @return Result of API execution status.
+ */
+struct bmp3_dev* get_bmp390_instance(void);
+
+#endif
---
+++ Core/Src/bmp390_runner.c
@@ -0,0 +1,109 @@
+/*******************************************************************************
+ * @file bmp390_runner.c
+ * @brief BMP390 HAL runner.
+ *******************************************************************************
+
+/** Includes. *****************************************************************/
+
+#include "bmp390_runner.h"
+
+#include <stdio.h>
+
+/** Public functions. *********************************************************/
+
+struct bmp3_dev *get_bmp390_instance(void) {
+  struct bmp3_dev dev;
+
+  uint16_t settings_sel;
+  uint16_t settings_fifo;
+  uint8_t fifo_data[FIFO_MAX_SIZE];
+  struct bmp3_fifo_settings fifo_settings = {0};
+  struct bmp3_settings settings = {0};
+  struct bmp3_fifo_data fifo = {0};
+
+  bmp3_interface_init(&dev, BMP3_I2C_INTF);
+
+  bmp3_init(&dev);
+
+  // Initalize FIFO settings.
+  fifo_settings.mode = BMP3_ENABLE;                       // Enable.
+  fifo_settings.press_en = BMP3_ENABLE;                   // Pressure.
+  fifo_settings.temp_en = BMP3_ENABLE;                    // Temperature.
+  fifo_settings.filter_en = BMP3_ENABLE;                  // Filtered.
+  fifo_settings.down_sampling = BMP3_FIFO_NO_SUBSAMPLING; // No downsample.
+  fifo_settings.ffull_en = BMP3_ENABLE;                   // FIFO full enable.
+  fifo_settings.time_en = BMP3_ENABLE;                    // Time enable.
+
+  // Initialze FIFO.
+  fifo.buffer = fifo_data;
+  fifo.req_frames = FIFO_FRAME_COUNT;
+
+  // Initialize general settings.
+  settings.press_en = BMP3_ENABLE; // Pressure.
+  settings.temp_en = BMP3_ENABLE;  // Temperature.
+  settings.odr_filter.press_os =
+      BMP3_NO_OVERSAMPLING; // No oversample pressure output data rate (ODR).
+  settings.odr_filter.temp_os =
+      BMP3_NO_OVERSAMPLING;                 // No oversample temperature ODR.
+  settings.odr_filter.odr = BMP3_ODR_50_HZ; // Base ORD of 50 Hz.
+
+  settings_sel = BMP3_SEL_PRESS_EN | BMP3_SEL_TEMP_EN | BMP3_SEL_PRESS_OS |
+                 BMP3_SEL_TEMP_OS | BMP3_SEL_ODR;
+  bmp3_set_sensor_settings(settings_sel, &settings, &dev);
+
+  // Set normal operation mode.
+  settings.op_mode = BMP3_MODE_NORMAL;
+  bmp3_set_op_mode(&settings, &dev);
+
+  settings_fifo = BMP3_SEL_FIFO_MODE | BMP3_SEL_FIFO_PRESS_EN |
+                  BMP3_SEL_FIFO_TEMP_EN | BMP3_SEL_FIFO_FULL_EN |
+                  BMP3_SEL_FIFO_DOWN_SAMPLING | BMP3_SEL_FIFO_FILTER_EN;
+  bmp3_set_fifo_settings(settings_fifo, &fifo_settings, &dev);
+  // TODO: Error handle via bmp3_get_fifo_settings(&fifo_settings, &dev);
+
+  ///////////////////////////////////////////////////////////////////////////
+  // TODO: WIP SPLIT
+  ///////////////////////////////////////////////////////////////////////////
+  // BMP3 driver variables.
+  uint16_t fifo_length = 0;
+  int8_t rslt;
+  struct bmp3_status status = {{0}};
+  struct bmp3_data fifo_p_t_data[FIFO_MAX_SIZE];
+
+  uint8_t try = 1;
+  uint8_t index = 0;
+
+  // Begin reading fifo full interrupt data.
+  while (try <= 10) {
+    rslt = bmp3_get_status(&status, &dev);
+
+    if ((rslt == BMP3_OK) && (status.intr.fifo_full == BMP3_ENABLE)) {
+      bmp3_get_fifo_length(&fifo_length, &dev);
+      bmp3_get_fifo_data(&fifo, &fifo_settings, &dev);
+
+      // Read status register again to clear FIFO Full interrupt status.
+      rslt = bmp3_get_status(&status, &dev);
+
+      if (rslt == BMP3_OK) {
+        rslt = bmp3_extract_fifo_data(fifo_p_t_data, &fifo, &dev);
+
+        for (index = 0; index < fifo.parsed_frames; index++) {
+#ifdef BMP3_FLOAT_COMPENSATION
+          printf("Frame[%d]  T: %.2f deg C, P: %.2f Pa\n", index,
+                 (fifo_p_t_data[index].temperature),
+                 (fifo_p_t_data[index].pressure));
+#else
+          printf("Frame[%d]  T: %ld deg C, P: %lu Pa\n", index,
+                 (long int)(int32_t)(fifo_p_t_data[index].temperature / 100),
+                 (long unsigned int)(uint32_t)(fifo_p_t_data[index].pressure /
+                                               100));
+#endif
+        }
+      }
+
+      try++;
+    }
+  }
+
+  return rslt;
+}
