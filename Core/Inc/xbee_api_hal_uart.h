a9890080b5b4767b4f67ed99f16d9debd150fe54
danielljeon <48095779+danielljeon@users.noreply.github.com>
2024-11-23 12:10:05 -0500

    Add XBee NRST pin definitions

--- Core/Inc/xbee_api_hal_uart.h
+++ Core/Inc/xbee_api_hal_uart.h
@@ -18,6 +18,10 @@ extern UART_HandleTypeDef huart1;
 // U(S)ART.
 #define XBEE_HUART huart1
 
+// GPIO output for reset.
+#define XBEE_NRST_PORT GPIOC
+#define XBEE_NRST_PIN GPIO_PIN_10
+
 /** Definitions. **************************************************************/
 
 #define DMA_RX_BUFFER_SIZE 256
