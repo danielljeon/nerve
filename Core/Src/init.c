f7757da74c39f132c263d59ffe483918ca48d941
danielljeon <48095779+danielljeon@users.noreply.github.com>
2025-04-21 01:51:13 -0400

    Decompose (CAN) telemetry and cleanup init

--- Core/Src/init.c
+++ Core/Src/init.c
@@ -7,6 +7,18 @@
 /** Includes. *****************************************************************/
 
 #include "init.h"
+#include "bmp390_runner.h"
+#include "bno085_runner.h"
+#include "can.h"
+#include "diagnostics.h"
+#include "rtc.h"
+#include "runcam_hal_uart.h"
+#include "scheduler.h"
+#include "sd.h"
+#include "telemetry.h"
+#include "ublox_hal_uart.h"
+#include "ws2812b_hal_pwm.h"
+#include "xbee_api_hal_uart.h"
 
 /** Definitions. **************************************************************/
 
@@ -21,6 +33,7 @@ FATFS SDFatFs; // Not following snake case conventions here, see below.
 //  be used instead.
 
 uint8_t xbee_sensor_data_transmit_index = 0;
+uint8_t can_sensor_data_transmit_index = 0;
 
 /** Private functions. ********************************************************/
 
@@ -136,50 +149,36 @@ void sequential_transmit_sensor_data(void) {
   xbee_sensor_data_transmit_index = (xbee_sensor_data_transmit_index + 1) % 8;
 }
 
-void can_transmit(void) {
-  // State.
-  can_message_t state_msg = dbc_messages[0];
-  uint32_t state_sigs[1] = {0}; // TODO: Hardcoded.
-  can_send_message_raw32(&hcan1, &state_msg, state_sigs);
-
-  // BMP390 barometric message.
-  can_message_t pressure_msg = dbc_messages[1];
-  uint32_t pressure_sigs[3] = {0};
-  const double pressure_source_sigs[3] = {bmp390_pressure, bmp390_temperature,
-                                          (double)bmp390_fault_count};
-  for (int i = 0; i < pressure_msg.signal_count; ++i) {
-    pressure_sigs[i] =
-        double_to_raw(pressure_source_sigs[i], &pressure_msg.signals[i]);
+void sequential_can_transmit(void) {
+  // Reset index if out of bounds.
+  if (can_sensor_data_transmit_index < 0 ||
+      can_sensor_data_transmit_index > 7) {
+    can_sensor_data_transmit_index = 0;
   }
-  can_send_message_raw32(&hcan1, &pressure_msg, pressure_sigs);
 
-  // GPS1.
-  can_message_t gps1_msg = dbc_messages[2];
-  uint32_t gps1_sigs[2] = {0};
-  const double gps1_source_sigs[2] = {gps_data.latitude, gps_data.longitude};
-  for (int i = 0; i < gps1_msg.signal_count; ++i) {
-    gps1_sigs[i] = double_to_raw(gps1_source_sigs[i], &gps1_msg.signals[i]);
+  switch (can_sensor_data_transmit_index) {
+  case 0:
+    can_tx_state();
+    break;
+  case 1:
+    can_tx_barometric();
+    break;
+  case 2:
+    can_tx_gps1();
+    break;
+  case 3:
+    can_tx_gps2();
+    break;
+  case 4:
+    can_tx_gps3();
+    break;
+  default:
+    can_sensor_data_transmit_index = 0;
+    break; // Unknown index.
   }
-  can_send_message_raw32(&hcan1, &gps1_msg, gps1_sigs);
 
-  // GPS2.
-  can_message_t gps2_msg = dbc_messages[3];
-  uint32_t gps2_sigs[2] = {0};
-  const double gps2_source_sigs[2] = {gps_data.altitude, 0}; // TODO: Hardcoded.
-  for (int i = 0; i < gps2_msg.signal_count; ++i) {
-    gps2_sigs[i] = double_to_raw(gps2_source_sigs[i], &gps2_msg.signals[i]);
-  }
-  can_send_message_raw32(&hcan1, &gps2_msg, gps2_sigs);
-
-  // GPS3.
-  can_message_t gps3_msg = dbc_messages[4];
-  uint32_t gps3_sigs[3] = {0};
-  const uint32_t gps3_source_sigs[3] = {gps_data.fix_quality,
-                                        gps_data.satellites, 0};
-  for (int i = 0; i < gps3_msg.signal_count; ++i) {
-    gps3_sigs[i] = uint_to_raw(gps3_source_sigs[i], &gps3_msg.signals[i]);
-  }
-  can_send_message_raw32(&hcan1, &gps3_msg, gps3_sigs);
+  // Increment the index and wrap around.
+  can_sensor_data_transmit_index = (can_sensor_data_transmit_index + 1) % 8;
 }
 
 /** Public functions. *********************************************************/
@@ -220,5 +219,5 @@ void nerve_init(void) {
   //  runs at main loop speed.
   scheduler_add_task(bmp390_get_data, 10);
   scheduler_add_task(sequential_transmit_sensor_data, 50);
-  scheduler_add_task(can_transmit, 50);
+  scheduler_add_task(sequential_can_transmit, 50);
 }
