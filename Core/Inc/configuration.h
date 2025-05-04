406d04396f1784ee63e1c44b3f15694cf44f3301
danielljeon <48095779+danielljeon@users.noreply.github.com>
2025-05-04 03:04:51 -0400

    Disable CAN telemetry flood definition by default

--- Core/Inc/configuration.h
+++ Core/Inc/configuration.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * @file configuration.h
+ * @file configuration.h <- TODO: DEV CONFIGURATIONS!
  * @brief High level hardcoded configuration related declarations/definitions.
  *******************************************************************************
  */
@@ -7,8 +7,9 @@
 #ifndef NERVE__CONFIGURATION_H
 #define NERVE__CONFIGURATION_H
 
-/** Definitions. **************************************************************/
+/** Configuration definitions. ************************************************/
 
-#define NERVE_DEBUG_FULL_CAN_TELEMETRY
+// Full telemetry flood on CAN bus intended for debug/development purposes.
+//#define NERVE_DEBUG_FULL_CAN_TELEMETRY
 
 #endif
